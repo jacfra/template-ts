082f27d73b9e7b584c2c9b0ed597ad63d69969d3
Jack Fraser <95544241+jacfra@users.noreply.github.com>
2022-11-29 10:30:57 -0600

    docker midway

--- server/src/database/DataSource.ts
+++ server/src/database/DataSource.ts
@@ -4,15 +4,16 @@ import { DemoMigration } from "../migration/DemoMigration";
 import { Log } from "../utility/Log/Logger";
 
 export let databaseSource = new DataSource({
-  name: "serve",
-  host: "localhost",
-  port: 5432,
-  username: "dev",
-  password: "devpw",
-  database: "devdb",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [DemoEntity],
   migrations: [DemoMigration],
   type: "postgres",
   logger: Log,
-  dropSchema: true,
+  dropSchema: false,
+  synchronize: false,
+  migrationsRun: true,
 });
